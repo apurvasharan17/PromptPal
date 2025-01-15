371931b8a71c359ae12115c739675a64e848e2b6
itsvaibhav30 <heyvaibhav03@gmail.com>
2025-01-15 21:25:57 +0530

    Final Changes

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ print("Current working directory", os.getcwd())
 app=create_app()
 
 if __name__=='__main__':
-    print("Flask app is running on http://127.0.0.1:5000/")
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(debug=True)
 
 
