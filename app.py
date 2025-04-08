e1cc5dd634314c1e6c541351a8c48cf5dca1c099
itsvaibhav30 <heyvaibhav03@gmail.com>
2025-04-08 23:17:26 +0530

    first commit

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ print("Current working directory", os.getcwd())
 app=create_app()
 
 if __name__=='__main__':
-    app.run(debug=True)
+    print("Flask app is running on http://127.0.0.1:5000/")
+    app.run(host='127.0.0.1', port=5000, debug=True)
 
 
